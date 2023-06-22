1f66858d02dc080b32db3600af9a1f1f2768ac04
Aldisti <alessandrodiste99@gmail.com>
2023-06-22 15:23:21 +0200

    Automated push by Aldisti

--- irc.hpp
+++ irc.hpp
@@ -6,7 +6,7 @@
 /*   By: adi-stef <adi-stef@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/21 11:21:38 by adi-stef          #+#    #+#             */
-/*   Updated: 2023/06/21 11:22:07 by adi-stef         ###   ########.fr       */
+/*   Updated: 2023/06/22 15:21:38 by adi-stef         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,16 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
+#include <vector>
+#include <cctype>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <errno.h>
+#include <stdio.h>
+#include <unistd.h>
 
+#define MYPORT "3490"  // the port users will be connecting to
+#define BACKLOG 10     // how many pending connections queue will hold
 
--- main.cpp
+++ main.cpp
@@ -6,14 +6,54 @@
 /*   By: adi-stef <adi-stef@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/21 11:17:48 by adi-stef          #+#    #+#             */
-/*   Updated: 2023/06/21 11:22:27 by adi-stef         ###   ########.fr       */
+/*   Updated: 2023/06/22 15:22:21 by adi-stef         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "irc.hpp"
 
-int	main(void)
+int main(void)
 {
-	
+    struct sockaddr_storage	their_addr;
+    socklen_t				addr_size;
+    struct addrinfo			hints, *res;
+    int						sockfd, new_fd, n;
+	char					buf[1000];
+
+    // !! don't forget your error checking for these calls !!
+    // first, load up address structs with getaddrinfo():
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;  // use IPv4 or IPv6, whichever
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;     // fill in my IP for me
+
+    if (getaddrinfo(NULL, MYPORT, &hints, &res) == -1)
+		perror("");
+
+    // make a socket, bind it, and listen on it:
+    sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+    bind(sockfd, res->ai_addr, res->ai_addrlen);
+    listen(sockfd, BACKLOG);
+
+    // now accept an incoming connection:
+
+    addr_size = sizeof their_addr;
+    new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &addr_size);
+
+    // ready to communicate on socket descriptor new_fd!
+
+	while (42)
+	{
+		n = recv(new_fd, buf, 1000, 0);
+		if (n < 0)
+			return (15);
+		if (!n)
+			break ;
+		std::cout << "Received: " << buf << std::endl;
+	}
+	std::cout << "Connection closed" << std::endl;
+	close(sockfd);
+	close(new_fd);
 	return (0);
 }
