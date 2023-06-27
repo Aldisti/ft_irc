f3a40cd9dc3bcde699b5fe346918ef08bfb83820
Giovanni Panico <gpanico@e3r3p5.42roma.it>
2023-06-27 11:54:32 +0200

    pseudo server

--- main.cpp
+++ main.cpp
@@ -1,92 +1,120 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: adi-stef <adi-stef@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/21 11:17:48 by adi-stef          #+#    #+#             */
-/*   Updated: 2023/06/26 16:25:11 by adi-stef         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <string>
+#include <string.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/poll.h>
+#include <sys/types.h>
 
-#include "irc.hpp"
-
-int main(void)
+int	main(void)
 {
-    struct sockaddr_storage	their_addr;
-    socklen_t				addr_size;
-    struct addrinfo			hints, *res;
-    int						sockfd, new_fd, n;
-	char					buf[1000];
-	struct pollfd			poll_s[50];
-	unsigned short			poll_n = 0;
+	struct addrinfo		hints;
+	struct addrinfo		*res;
+	struct sockaddr		their_addr;
+	socklen_t		sin_addr = sizeof(their_addr);
+	struct pollfd		pollfds[50];
+	char			buff[1001];
+	int			buff_size = 1000;
+	int			npollfds = 1;
+	int			err;
+	int			sfd;
+	int			rs;
+	int			r;
 
-    // !! don't forget your error checking for these calls !!
-    // first, load up address structs with getaddrinfo():
-
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_UNSPEC;  // use IPv4 or IPv6, whichever
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE;     // fill in my IP for me
-
-    if (getaddrinfo(NULL, MYPORT, &hints, &res) == -1)
-		perror("");
-
-    // make a socket, bind it, and listen on it:
-    sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-    bind(sockfd, res->ai_addr, res->ai_addrlen);
-
-    listen(sockfd, BACKLOG);
-
-	poll_s[0].fd = sockfd;
-	poll_s[0].events = POLLIN;
-	poll_s[0].revents = 0;
-	poll_n++;
-
-    // now accept an incoming connection:
-
-    // addr_size = sizeof their_addr;
-    // new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &addr_size);
-
-	while (42)
+	memset((void *) &hints, 0, sizeof(hints));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = 0;
+	hints.ai_flags = AI_PASSIVE;
+	err = getaddrinfo(NULL, "8000", &hints, &res);
+	if (err)
 	{
-		std::cout << "Polling..." << std::endl;
-		switch (poll(poll_s, poll_n, 1000))
+		std::cerr << "getaddrinfo() failed" << std::endl;
+		return (err);
+	}
+	sfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+	if (sfd == -1)
+	{
+		std::cerr << "socket() failed" << std::endl;
+		return (1);
+	}
+	if (bind(sfd, res->ai_addr, res->ai_addrlen))
+	{
+		std::cerr << "bind() failed" << std::endl;
+		return (1);
+	}
+	freeaddrinfo(res);
+	if (listen(sfd, 50))
+	{
+		std::cerr << "listen() failed" << std::endl;
+		close(sfd);
+		return (1);
+	}
+	memset((void *) pollfds, 0, sizeof(pollfds));
+	pollfds[0].fd = sfd;
+	pollfds[0].events = POLLIN;
+	for (int k = 0; k < 100; k++)
+	{
+		std::cout << "polling..." << std::endl;
+		rs = poll(pollfds, npollfds, 1000);
+		if (rs < 0)
 		{
-			case -1:
-				std::cout << "poll error" << std::endl;
-				return (15);
-				break;
-			case 0:
+			std::cerr << "poll() failed" << std::endl;
+			close(sfd);
+			return (1);
+		}
+		else if (rs == 0)
+			continue ;
+		for (int i = 0; i < npollfds && rs > 0; i++)
+		{
+			if (pollfds[i].revents != POLLIN || pollfds[i].fd == -1)
 				continue ;
-		}
-		std::cout << "poll_s[0].revents == " << poll_s[0].revents << std::endl;
-		if (poll_s[0].revents == POLLIN)
-		{
-			addr_size = sizeof their_addr;
-			std::cout << "Accepting..." << std::endl;
-    		new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &addr_size);
-			std::cout << "Accept done" << std::endl;
-			poll_s[poll_n].fd = new_fd;
-			poll_s[poll_n].events = POLLIN | POLLOUT;
-			poll_s[poll_n].revents = 0;
-			poll_n++;
-			// continue ;
-		}
-		for (int i = 1; i < poll_n; i++)
-		{
-			std::cout << "fd: " << poll_s[i].fd << " - " << poll_s[i].revents << std::endl;
-			if (poll_s[i].revents == POLLOUT)
+			std::cout << "POLLIN" << std::endl;
+			if (pollfds[i].fd == sfd)
 			{
-				std::cout << "Receiving: " << i << std::endl;
-				n = recv(new_fd, buf, 1000, MSG_DONTWAIT);
-				std::cout << "Received: " << buf << std::endl;
+				pollfds[npollfds].fd = accept(sfd, &their_addr, &sin_addr);
+				if (pollfds[npollfds].fd == -1)
+				{
+					std::cerr << "accept() failed" << std::endl;
+					pollfds[npollfds].fd = 0;
+					continue ;
+				}
+				pollfds[npollfds].events = POLLIN;
+				npollfds++;
+			}
+			else
+			{
+				memset((void *) buff, 0, buff_size + 1);
+				r = recv(pollfds[i].fd, buff, buff_size, MSG_DONTWAIT);
+				if (r < 0)
+				{
+					std::cerr << "recv() failed" << std::endl;
+					close(pollfds[i].fd);
+					pollfds[i].fd = -1;
+					continue ;
+				}
+				else if (r == 0)
+				{
+					std::cerr << "connection closed" << std::endl;
+					close(pollfds[i].fd);
+					pollfds[i].fd = -1;
+					continue ;
+				}
+				if (npollfds > 2)
+				{
+					if (i + 1 != npollfds)
+						send(pollfds[i + 1].fd, buff, buff_size, MSG_DONTWAIT);
+					else
+						send(pollfds[i - 1].fd, buff, buff_size, MSG_DONTWAIT);
+				}
+				else
+					send(pollfds[i].fd, buff, buff_size, MSG_DONTWAIT);
 			}
 		}
 	}
-	std::cout << "Connection closed" << std::endl;
-	close(sockfd);
-	close(new_fd);
+	for (int i = 0; i < npollfds; i++)
+		if (pollfds[i].fd != -1)
+			close(pollfds[i].fd);
 	return (0);
 }
