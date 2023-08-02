59112e53b57ed3e34f4b0df6cd02371316b893f1
Aldisti <alessandrodiste99@gmail.com>
2023-08-02 09:59:45 +0200

    parse done

--- tmp.cpp
+++ tmp.cpp
@@ -6,51 +6,58 @@
 /*   By: adi-stef <adi-stef@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/30 12:17:25 by adi-stef          #+#    #+#             */
-/*   Updated: 2023/06/30 17:24:21 by adi-stef         ###   ########.fr       */
+/*   Updated: 2023/08/02 09:59:35 by adi-stef         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "irc.hpp"
 
-void	parse(std::string line)
+std::vector<std::string>	ft_parse(std::string line)
 {
 	std::vector<std::string>	arr;
 	int		i = 0;
 	int		j = 0;
 	int		end = 0;
 
-	// if (line.back() != 10)
-	// 	return ;
-	// std::cout << line << std::endl;
 	if (line[0] == ':')
 	{
-		arr.push_back(line.substr(1, line.find(32) - 1));
-		i = line.find(32);
-		while (line[i] == 32 && i < (int)line.size()) i++;
-		j = i++;
+		i = line.find(' ');
+		j++;
+		arr.push_back(line.substr(j, i - j));
+		j = ++i;
 	}
 	end = (line.find(':', i) != NPOS) ? line.find(':', i) : (int)line.size();
-	// std::cout << "end: " << end << std::endl;
 	while (i < end)
 	{
-		i = line.find(32, j);
-		if (i == NPOS)
+		if ((i = line.find(' ', j)) == NPOS)
 			i = line.size();
 		arr.push_back(line.substr(j, i - j));
-		while (line[i] == 32 && i < (int)line.size()) i++;
-		j = i++;
+		j = ++i;
 	}
-	// std::cout << "Ciclo finito con: [" << j << "] [" << i << "]" << std::endl;
-	if (line[j] == ':')
-		arr.push_back(line.substr(i, line.size()));
-	for (int i = 0; i < (int)arr.size(); i++)
-		std::cout << "|" << arr[i] << "| ";
-	std::cout << std::endl;
+	if (j < (int)line.size() && line[j] == ':')
+		arr.push_back(line.substr(i));
+
+	if (arr.size() == 0 || arr.size() > 17)
+		throw std::exception();
+	else if (arr.size() > 16 && (end == (int)line.size() || line[0] != ':'))
+		throw std::exception(); // throw exception
+	else if (arr.size() > 15 && (end == (int)line.size() && line[0] != ':'))
+		throw std::exception(); // throw exception
+	for (int i = 0; i < (int)arr.size(); i++) {
+		if (!arr[i].size())
+			throw std::exception(); // throw exception
+		if ((arr[i].find(' ') != NPOS && arr[i] != arr.back()) || (arr[i].find(' ') != NPOS && arr[i] == arr.back() && end == (int)line.size()))
+			throw std::exception(); // throw exception
+	}
+	return (arr);
 }
 
 int	main(void)
 {
-	parse(":fiffo ciao     come  stai :a b c");
+	parse(":fiffo 1 2");
+	parse(":fiffo 1 2 3 4 5 :ciao come stai");
+	parse(":fiffo 0 1 2 3 4 5 6 7 8 9 1 2 3 4 5 :ciao come stai");
+	parse("");
 	//     01234567890123456789012345678901234
 	return (0);
 }
